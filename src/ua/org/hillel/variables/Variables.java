c9068581f48b3378e7882d4cb665dfe2fde1e990
Andrey Zolotarev <Zolotarev@3g.ua>
2016-08-11 20:52:21 +0300

    first commit

---
+++ src/ua/org/hillel/variables/Variables.java
@@ -0,0 +1,19 @@
+package ua.org.hillel.variables;
+
+/**
+ * Created by ITSCHOOL2 on 11.08.2016.
+ */
+public class Variables {
+
+    public static void main (String [] args ){
+
+System.out.println(" Second lesson - VARIABLES ");
+
+        // comment
+
+        //   byte b = 0;
+
+
+
+    }
+}
